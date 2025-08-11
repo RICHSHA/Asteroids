b979aa12be3cae952e44bc226e262326f54e3718
RICSHA <lovro.saric@outlook.com>
2025-08-11 10:42:28 +0200

    added ability for player to rotate

--- main.py
+++ main.py
@@ -20,12 +20,9 @@ def main():
     
         screen.fill((0, 0, 0))  # Fill the screen with black
         player.draw(screen)
+        player.update(dt)
+        
         pygame.display.flip()  # Update the display
-
-
-
-
-
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
