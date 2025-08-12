75ed2636649e34af42bf37a2ebb309a7033c1b5e
RICSHA <lovro.saric@outlook.com>
2025-08-12 10:38:50 +0200

    asteroids now split when destroyed

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import CircleShape
 import pygame
+from constants import ASTEROID_MIN_RADIUS
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,17 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
+    def split(self):
+        self.kill()
+        if self.radius == ASTEROID_MIN_RADIUS:
+            return
+        else:
+            angle = random.uniform(20, 50)
+            v1 = self.velocity.rotate(angle)
+            v2 = self.velocity.rotate(-angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            a1 = Asteroid(self.position[0], self.position[1], new_radius)
+            a2 = Asteroid(self.position[0], self.position[1], new_radius)
+            a1.velocity = v1 * 1.2
+            a2.velocity = v2 * 1.2
