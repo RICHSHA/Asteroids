783135bdc854fcc5ef1f5567d210e8cd7836a8b7
RICSHA <lovro.saric@outlook.com>
2025-08-12 10:30:06 +0200

    added ability to shoot and kill asteroids

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
+import sys
 
 def main():
     # print("Starting Asteroids!")
@@ -15,10 +17,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -33,11 +37,26 @@ def main():
     
         screen.fill((0, 0, 0))  # Fill the screen with black
   
+        # draw all the objects
         for d in drawable:
             d.draw(screen)
         updatable.update(dt)
         
-        pygame.display.flip()  # Update the display
+        # Update the display
+        pygame.display.flip()  
+
+        
+        for a in asteroids:
+            if a.collision(player):
+                print('Game over!')
+                sys.exit()
+            
+            for s in shots:
+                if a.collision(s):
+                    a.split()
+                    s.kill()
+        
+
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
+from shot import Shot
 import pygame
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.rate_limit = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -32,7 +35,17 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+            
+        self.rate_limit -= dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        if self.rate_limit <= 0:
+            shot = Shot(self.position[0], self.position[1])
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.rate_limit = PLAYER_SHOOT_COOLDOWN
